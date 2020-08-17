0617c36197ef57746da87b133e4c772af422ab9a
joseph.li <joseph.li@yingzt.com>
2020-08-17 18:03:30 +0800

    int to roman

---
+++ src/main/java/com/lee/owner/leetcode/IntToRomanSolution.java
@@ -0,0 +1,86 @@
+package com.lee.owner.leetcode;
+
+/**
+ * @author joseph.li
+ * @date 2020/8/17 5:30 下午
+ */
+public class IntToRomanSolution {
+
+    public static void main(String[] args) {
+        System.out.println(new IntToRomanSolution().intToRoman(60));
+    }
+
+    private static final int NINE = 9;
+
+    private static final int FOUR = 4;
+
+    private static final int FIVE = 5;
+
+    public String intToRoman(int num) {
+        int x = num / 1000;
+        StringBuilder stringBuilder = new StringBuilder();
+        if (x > 0) {
+            for (int i = 0; i < x; i++) {
+                stringBuilder.append("M");
+            }
+            num = num - x * 1000;
+        }
+
+        x = num / 100;
+        if (x > 0) {
+            if (x == NINE) {
+                stringBuilder.append("CM");
+            } else if (x >= FIVE) {
+                stringBuilder.append("D");
+                for (int i = 0; i < x - FIVE; i++) {
+                    stringBuilder.append("C");
+                }
+            } else if (x == FOUR) {
+                stringBuilder.append("CD");
+            } else {
+                for (int i = 0; i < x; i++) {
+                    stringBuilder.append("C");
+                }
+            }
+            num = num - x * 100;
+        }
+
+        x = num / 10;
+        if (x > 0) {
+            if (x == NINE) {
+                stringBuilder.append("XC");
+            } else if (x >= FIVE) {
+                stringBuilder.append("L");
+                for (int i = 0; i < x - FIVE; i++) {
+                    stringBuilder.append("X");
+                }
+            } else if (x == FOUR) {
+                stringBuilder.append("XL");
+            } else {
+                for (int i = 0; i < x; i++) {
+                    stringBuilder.append("X");
+                }
+            }
+            num = num - x * 10;
+        }
+
+        if (num > 0) {
+            if (num == NINE) {
+                stringBuilder.append("IX");
+            } else if (num >= FIVE) {
+                stringBuilder.append("V");
+                for (int i = 0; i < num - FIVE; i++) {
+                    stringBuilder.append("I");
+                }
+            } else if (num == FOUR) {
+                stringBuilder.append("IV");
+            } else {
+                for (int i = 0; i < num; i++) {
+                    stringBuilder.append("I");
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+}
